864c029280e3480f3195c952467dee638e84b11e
diridon <maksim.czedov.99@gmail.com>
2019-12-01 21:51:03 +0200

    Add project files.

---
+++ B-Tree Preorder Traversal/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace B_Tree_Preorder_Traversal
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        private IList<int> PreorderTraversal(TreeNode root)
+        {
+            IList<int> resultList = new List<int>();
+
+            if (root == null)
+            {
+                return resultList;
+            }
+
+            var nodeBuffer = new Stack<TreeNode>();
+            nodeBuffer.Push(root);
+            while (nodeBuffer.Count != 0)
+            {
+                root = nodeBuffer.Pop();
+                resultList.Add(root.Val);
+                if (root.Left != null) nodeBuffer.Push(root.Left);
+                if (root.Right != null) nodeBuffer.Push(root.Right);
+            }
+
+            return resultList;
+        }
+    }
+}
---
+++ B-Tree Preorder Traversal/TreeNode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace B_Tree_Preorder_Traversal
+{
+    public class TreeNode : ICloneable
+    {
+        public int Val;
+        public TreeNode Left;
+        public TreeNode Right;
+
+        public TreeNode(int x)
+        {
+            Val = x;
+        }
+
+        public object Clone()
+        {
+            TreeNode clone = new TreeNode(this.Val);
+            clone.Left = this.Left;
+            clone.Right = this.Right;
+            return clone;
+        }
+    }
+}
