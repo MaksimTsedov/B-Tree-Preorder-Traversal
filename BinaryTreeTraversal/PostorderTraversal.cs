d6d7223ee8c780175824ac492215c0b031e8fb73
diridon <maksim.czedov.99@gmail.com>
2019-12-09 08:38:35 +0200

    Added postorder traversal

---
+++ BinaryTreeTraversal/PostorderTraversal.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace BinaryTreeTraversal
+{
+    public class PostorderTraversal : ITraversal
+    {
+        public IList<int> TraversalStrategy(TreeNode root)
+        {
+            var resultList = new List<int>();
+            var nodeBuffer = new Stack<TreeNode>();
+
+            nodeBuffer.Push(root);
+            while (nodeBuffer.Count != 0)
+            {
+                root = nodeBuffer.Pop();
+                resultList.Add(root.Val);
+                if (root.Left != null) nodeBuffer.Push(root.Left);
+                if (root.Right != null) nodeBuffer.Push(root.Right);
+            }
+
+            resultList.Reverse();
+
+            return resultList;
+        }
+    }
+}
