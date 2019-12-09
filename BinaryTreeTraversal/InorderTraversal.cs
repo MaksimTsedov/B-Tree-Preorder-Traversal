8edf4fc3a796be0c9b4ebdb73eba32e19758df2c
diridon <maksim.czedov.99@gmail.com>
2019-12-09 08:32:27 +0200

    Added inorder traversal implementation

---
+++ BinaryTreeTraversal/InorderTraversal.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace BinaryTreeTraversal
+{
+    public class InorderTraversal : ITraversal
+    {
+        public IList<int> TraversalStrategy(TreeNode root)
+        {
+            IList<int> resultList = new List<int>();
+
+            var nodeBuffer = new Stack<TreeNode>();
+            while (nodeBuffer.Count != 0 || root != null)
+            {
+                if (root != null)
+                {
+                    nodeBuffer.Push(root);
+                    root = root.Left;
+                }
+                else
+                {
+                    root = nodeBuffer.Pop();
+                    resultList.Add(root.Val);
+                    root = root.Right;
+                }
+            }
+
+            return resultList;
+        }
+    }
+}
